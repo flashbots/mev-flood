8b798bdc814bbd03d7066c7e685b60fb3b3a82ff
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2022-08-23 19:09:24 -0700

    send contract tx in sendPrivateTx script

--- src/scripts/sendPrivateTx.ts
+++ src/scripts/sendPrivateTx.ts
@@ -1,19 +1,23 @@
 import { FlashbotsBundleProvider, FlashbotsPrivateTransactionResponse, RelayResponseError } from '@flashbots/ethers-provider-bundle';
 import { BigNumber } from 'ethers';
+import contracts, { getContract } from '../lib/contracts';
 import env from '../lib/env';
 import { GWEI, PROVIDER } from '../lib/helpers';
 import { getAdminWallet } from '../lib/wallets';
 
 async function main() {
-    const adminWallet = getAdminWallet()
+    const adminWallet = getAdminWallet().connect(PROVIDER)
     const flashbotsProvider = await FlashbotsBundleProvider.create(PROVIDER, adminWallet, env.MEV_GETH_HTTP_URL, env.CHAIN_NAME)
+    const contract = getContract(contracts.LotteryMEV)
     const tx = {
+        ...contract.populateTransaction.bid(),
         from: adminWallet.address,
         to: adminWallet.address,
-        value: "0x42",
-        gasPrice: GWEI.mul(10),
-        gasLimit: BigNumber.from(21000),
+        value: GWEI.mul(1000),
+        gasPrice: GWEI.mul(50),
+        gasLimit: BigNumber.from(90000),
         chainId: env.CHAIN_ID,
+        nonce: await adminWallet.getTransactionCount()
     }
     const privateTx = {
         transaction: tx,
@@ -23,6 +27,8 @@ async function main() {
     const res: FlashbotsPrivateTransactionResponse | RelayResponseError = await flashbotsProvider.sendPrivateTransaction(privateTx)
     if ('wait' in res) {
         console.log("private tx res", res)
+        const simRes = await res.simulate()
+        console.log("sim result", simRes)
     } else {
         console.error("[privateTx] error", res)
     }
