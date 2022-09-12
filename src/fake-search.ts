4baeb458be6fc29ddc3ea66168ff5c4808fc0c78
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2022-09-12 11:20:59 -0700

    add finished fake-dev test

--- src/fake-search.ts
+++ src/fake-search.ts
@@ -1,12 +1,12 @@
 import { createNonLandingTx } from './lib/lottery'
-import { getWalletSet } from './lib/wallets'
 import { calculateBundleHash, PROVIDER } from './lib/helpers'
 import { sendBundle, simulateBundle } from './lib/flashbots'
-import { useMempool } from './lib/cliArgs'
 
-const doStuff = async (blockNum: number) => {
+const sendRevertingBundle = async (blockNum: number) => {
     console.log(`[BLOCK ${blockNum}]`)
-    const tx = await createNonLandingTx(1663017869)
+    const simBlock = blockNum - 1
+    const targetBlock = blockNum + 2
+    const tx = await createNonLandingTx(2000000000)
     const bundle = tx ? [tx] : []
     if (!tx) {
         console.warn("fake tx is undefined")
@@ -14,25 +14,18 @@ const doStuff = async (blockNum: number) => {
     }
     const bundleHash = calculateBundleHash(bundle)
     console.log("bundleHash (pre-calculated)", bundleHash)
-    // simulate
+    
+    // simulate disabled for now...
     // console.log('simulating bundle...')
-    // const simResult = simulateBundle(bundle, blockNum - 1)
+    // const simResult = simulateBundle(bundle, simBlock)
     // console.log("simResult", await simResult)
+    
     // send
-    // console.log(tx)
-    console.log("sending bundle", bundle)
-    const sendResult = sendBundle(bundle, blockNum + 2)
-    console.log(sendResult)
+    console.log(`sending bundle, targeting block ${targetBlock}...`)
+    const sendResult = sendBundle(bundle, targetBlock)
     console.log(await sendResult)
 }
 
-// PROVIDER.on('block', async (blockNum: number) => {
-//     await doStuff(blockNum)
-// })
-
-const singleTest = async () => {
-    const blockNum = await PROVIDER.getBlockNumber()
-    await doStuff(blockNum)
-}
-
-singleTest()
+PROVIDER.on('block', async (blockNum: number) => {
+    await sendRevertingBundle(blockNum)
+})
