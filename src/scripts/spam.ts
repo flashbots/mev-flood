506c5e0b8f4f76fe437dce6ccb35559c34c32918
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2023-04-05 22:15:44 -0700

    decouple spam tx logic from controller loop

--- src/scripts/spam.ts
+++ src/scripts/spam.ts
@@ -11,27 +11,31 @@ const sleep = (ms: number) => {
 
 const {wallet, bundlesPerSecond, txsPerBundle, sendRoute, overdrive} = getSpamArgs()
 
-const spam = async () => {
+const spam = async (mevFlood: MevFlood, targetBlockNumber: number, virtualNonce: number) => {
+    // calling generateSwaps with only one wallet will produce a bundle with only one tx
+    const txBundles = await Promise.all(Array(txsPerBundle).fill(0).map((_, idx) => mevFlood.generateSwaps({}, [wallet], virtualNonce + idx)))
+    virtualNonce = virtualNonce + txBundles.map(b => b.swaps.signedSwaps.length).reduce((a, b) => a + b, 0)
+    const bundle = txBundles.map(txb => txb.swaps.signedSwaps.map(s => s.signedTx)).flat()
+
+    if (sendRoute === SendRoute.Mempool) {
+        mevFlood.sendToMempool(bundle).catch(() => {/* completely ignore errors */})
+    } else if (sendRoute === SendRoute.MevShare) {
+        mevFlood.sendToMevShare(bundle, {hints: {calldata: true, logs: true}}).catch(() => {/* completely ignore errors */})
+    } else {
+        mevFlood.sendBundle(bundle, targetBlockNumber).catch(() => {/* completely ignore errors */})
+    }
+}
+
+async function main() {
     const mevFlood = await (
-        await new MevFlood(wallet, PROVIDER).withDeploymentFile(await getExistingDeploymentFilename())
+        await new MevFlood(wallet, PROVIDER)
+        .withDeploymentFile(await getExistingDeploymentFilename())
     ).initFlashbots(getAdminWallet())
-    let virtualNonce = await wallet.connect(PROVIDER).getTransactionCount()
-    let lastBlockSampledAt = now()
     let targetBlockNumber = await PROVIDER.getBlockNumber() + 1
+    let virtualNonce = await wallet.connect(PROVIDER).getTransactionCount()
+
     while (true) {
-        // calling generateSwaps with only one wallet will produce a bundle with only one tx
-        const txBundles = await Promise.all(Array(txsPerBundle).fill(0).map((_, idx) => mevFlood.generateSwaps({}, [wallet], virtualNonce + idx)))
-        virtualNonce += txBundles.map(b => b.swaps.signedSwaps.length).reduce((a, b) => a + b, 0)
-        const bundle = txBundles.map(txb => txb.swaps.signedSwaps.map(s => s.signedTx)).flat()
-
-        if (sendRoute === SendRoute.Mempool) {
-            mevFlood.sendToMempool(bundle).catch(() => {/* completely ignore errors */})
-        } else if (sendRoute === SendRoute.MevShare) {
-            mevFlood.sendToMevShare(bundle, {hints: {calldata: true, logs: true}}).catch(() => {/* completely ignore errors */})
-        } else {
-            mevFlood.sendBundle(bundle, targetBlockNumber).catch(() => {/* completely ignore errors */})
-        }
-
+        spam(mevFlood, targetBlockNumber, virtualNonce)
         await sleep(1000 / bundlesPerSecond)
         if (now() - lastBlockSampledAt > 12000) {
             targetBlockNumber += 1
@@ -40,8 +44,9 @@ const spam = async () => {
     }
 }
 
+let lastBlockSampledAt = now()
 for (let i = 0; i < overdrive; i++) {
-    spam().then(() => {
+    main().then(() => {
         process.exit(0)
     })
 }
