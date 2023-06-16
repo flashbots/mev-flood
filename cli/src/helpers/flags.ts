d74b9b05f648d0d2e1100448bb4906e1bb74cd18
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2023-06-16 14:26:28 -0700

    lint cli (cleans up diff for upcoming PR)

--- cli/src/helpers/flags.ts
+++ cli/src/helpers/flags.ts
@@ -1,16 +1,16 @@
-import { Flags } from '@oclif/core';
+import {Flags} from '@oclif/core'
 
 export const floodFlags = {
-    rpcUrl: Flags.string({
-        char: 'r',
-        description: 'HTTP JSON-RPC endpoint.',
-        required: false,
-        default: "http://localhost:8545"
-    }),
-    privateKey: Flags.string({
-        char: 'k',
-        description: "Private key used to send transactions and deploy contracts.",
-        required: false,
-        default: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-    }),
+  rpcUrl: Flags.string({
+    char: 'r',
+    description: 'HTTP JSON-RPC endpoint.',
+    required: false,
+    default: 'http://localhost:8545',
+  }),
+  privateKey: Flags.string({
+    char: 'k',
+    description: 'Private key used to send transactions and deploy contracts.',
+    required: false,
+    default: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+  }),
 }
